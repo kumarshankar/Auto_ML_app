2a713c619cdcbcd5ab7ea5d28a28d35d32864474
shankar kumar <kumarshankar.1992@gmail.com>
2021-05-04 13:42:16 +0530

    first_commit

---
+++ model.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error,r2_score
+from sklearn.datasets import load_diabetes, load_boston
+
